ae9df37a27b5f26d3a1005265509f62203d65b84
jorovipe97 <jorovipe@gmail.com>
2017-10-07 08:52:21 -0500

    Time and date dialog picker added.

---
+++ app/src/main/java/com/romualdo/ble/gattclient/SelectTimeFragment.java
@@ -0,0 +1,130 @@
+package com.romualdo.ble.gattclient;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.icu.text.DateFormat;
+import android.icu.util.Calendar;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SelectTimeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SelectTimeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SelectTimeFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public SelectTimeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SelectTimeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SelectTimeFragment newInstance(String param1, String param2) {
+        SelectTimeFragment fragment = new SelectTimeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstance) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+    }
+
+    // Called when user set a time
+    public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfDay) {
+
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    // This method causes app stoping
+    /*
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }*/
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
--- app/src/main/java/com/romualdo/ble/gattclient/SetAlarmActivity.java
+++ app/src/main/java/com/romualdo/ble/gattclient/SetAlarmActivity.java
@@ -1,7 +1,9 @@
 package com.romualdo.ble.gattclient;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SetAlarmActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class SetAlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_alarm);
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment fragment = new SelectTimeFragment();
+        fragment.show(getSupportFragmentManager(), "TimePicker");
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment fragment = new SelectDateFragment();
+        fragment.show(getSupportFragmentManager(), "DatePicker");
     }
 }
